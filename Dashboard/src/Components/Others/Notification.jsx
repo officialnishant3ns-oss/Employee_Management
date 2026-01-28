d47e96b25debd47f9573d80539918a7c19ad2768
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-01-28 23:44:16 +0530

    added new task ui

--- Dashboard/src/Components/Others/Notification.jsx
+++ Dashboard/src/Components/Others/Notification.jsx
@@ -1,11 +1,13 @@
 
 import React from 'react'
 import AcceptedTask from '../TaskItems/AcceptedTask'
+import NewTask from '../TaskItems/NewTask'
 
 const Notification = ({ className }) => {
   return (
     <div>
          <AcceptedTask/>
+         <NewTask />
     </div>
   )
 }
